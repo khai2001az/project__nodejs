b3c2dc8963427cbb0d012a9cc8a24a1e659f0a2f
khai2001az <01658904266az@gmail.com>
2021-05-29 16:48:40 +0700

    git+https://github.com/khai2001az/project__nodejs.git

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars')
+const path = require('path');
+const app = express();
+const port = 3000;
+
+//http logger
+app.use(morgan('combined'));
+
+//templae engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
