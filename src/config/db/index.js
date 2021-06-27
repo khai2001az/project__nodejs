03132ccfb4c750e3f0cd1e34b6f3118a451f570d
khai2001az <01658904266az@gmail.com>
2021-06-28 01:25:45 +0700

    main

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,18 +1,19 @@
 const mongoose = require ('mongoose');
-async function connect(){
+// async function connect(){
 
-    try{
-        await mongoose.connect('mongodb://localhost:27017/marvel_toy_dev', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+    // try{
+    //     await mongoose.connect('mongodb://localhost:27017/marvel_toy_dev', {
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true
+    //     });
 
-        console.log('Connect OKE')
-    }
-    catch(error){
-        console.log('Connect Not OKE')
-    }
+    //     console.log('Connect OKE')
+    // }
+    // catch(error){
+    //     console.log('Connect Not OKE')
+    // }
+    mongoose.connect('mongodb://localhost:27017/marvel_toy_dev').catch(err);
     
-}
+// }
 
 module.exports = { connect };
