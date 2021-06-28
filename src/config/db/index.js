08991c0f1b1b11417b2c3f7b926e948da7727ca9
khai2001az <01658904266az@gmail.com>
2021-06-28 12:09:25 +0700

    main

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,6 +4,8 @@ async function connect(){
     try{
         await mongoose.connect('mongodb://localhost:27017/marvel_toy_dev', {
             useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
             useUnifiedTopology: true
         });
 
